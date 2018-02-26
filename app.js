d1dd6b4544b90cbb0a693365db66cb631e00c9e0
Prateek Nagar <prateek@Blink.local>
2018-02-26 14:24:36 -0500

    Added simple routing and verfied on ARC

--- app.js
+++ app.js
@@ -3,4 +3,17 @@ var app = express();
 app.get('/', (req, res) => {
     res.send('Hello Express')
 });
+
+app.post('/', function(req, res) {
+	res.send("Routed from post");
+});
+
+app.put('/putReq', function(req, res) {
+	res.send('Routed from a put with the route /putReq');
+});
+
+app.delete('/deleteReq', function(rea, res) {
+	res.send("Routed from a delete with the route /deleteReq");
+});
+
 app.listen(2000)
